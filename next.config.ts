18e39b8ea68bfcb5efdbf886351b331135101c6e
Shozib Sayyed <shozibabbas@gmail.com>
2025-10-13 16:08:34 +0500

    remove eslint for poc

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
             bodySizeLimit: '100mb',
         },
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
