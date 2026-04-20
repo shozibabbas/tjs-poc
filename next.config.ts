9adb8e1c1fa380c443b7764f14e19b1edf53571a
Shozib Sayyed <shozibabbas@gmail.com>
2026-04-20 09:54:40 +0500

    chore: update dependencies and improve TypeScript configuration
    - upgraded Next.js from 15.5.15 to 16.2.4
    - upgraded eslint-config-next from 15.5.15 to 16.2.4
    - modified tsconfig.json to use "react-jsx" for JSX and improved formatting for "lib" and "paths"
    - added ".next/dev/types/**/*.ts" to the include paths in tsconfig.json

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
             bodySizeLimit: '100mb',
         },
     },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
 };
 
 export default nextConfig;
